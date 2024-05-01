ef1a97ae01bc8d7b5c2023ceabfdc1e2b9bdd78e
Andrey <andrey.smol5@gmail.com>
2024-05-01 18:12:07 +0300

    filters upd: fixed bugs with None in GET queries

--- startrix_back/women/views.py
+++ startrix_back/women/views.py
@@ -3,9 +3,15 @@ from rest_framework import generics, viewsets, pagination
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.db.models import Q
 
 from .models import *
 from .serializers import *
+
+
+class AllSourcePagination(pagination.PageNumberPagination):
+    page_size = 50
+    page_size_query_param = 'page_size'
 
 
 # Класс, где происходит фильтрация по языку программирования, полученному в запросе с фронта. Результат из GitHub
@@ -24,10 +30,24 @@ class AllSourceCityViewSet(viewsets.ReadOnlyModelViewSet):
         city = self.request.query_params.getlist('city')
         language = self.request.query_params.getlist("language")
         job = self.request.query_params.get("job")
-        queryset = AllSourceUsersEnriched.objects.filter(city__in=city, jobtitle__icontains=job,
-                                                         programming_languages__contains=language)
-        return queryset
+        queryset = AllSourceUsersEnriched.objects.all()
 
+        # Формируем условия для фильтрации
+        conditions = Q()  # Пустой Q-объект
+
+        if city:
+            conditions &= Q(city__in=city)
+        if job:
+            conditions &= Q(jobtitle__icontains=job)
+        if language:
+            conditions &= Q(programming_languages__contains=language)
+
+        # Применяем условия к queryset
+        if conditions:
+            queryset = queryset.filter(conditions)
+
+        return queryset
+    pagination_class = AllSourcePagination
     serializer_class = AllSourceSerializer
 
 
